7a4999cc5f1ac94be56c7f7efc0d26e9138c0489
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-12 19:10:14 +0300

    Добавьте файлы проекта.

---
+++ PrscticWork-6.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    
+    cout << "Введите число: ";
+    cin >> number;
+    
+    int count = 0;
+    if (number <= 0) count++; //минус тоже место на бумаге занимает
+
+    while (number != 0) {
+        number /= 10;
+        count++;
+    }
+
+    cout << "Цифр в числе: " << count << endl;
+
+    return 0;
+}
